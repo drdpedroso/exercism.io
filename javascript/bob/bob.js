273b093854a5d472e86507e4f375566d708d5bcd
Eduardo Pedroso <drdpedroso@gmail.com>
2016-11-06 23:46:38 -0200

    Bob first try

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,14 +1,18 @@
-//
-// This is only a SKELETON file for the "Bob" exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+  if(input[input.length - 1] == '?' && input != input.toUpperCase()){
+    return 'Sure.';
+  } else if (input === input.toUpperCase()) {
+    return 'Whoa, chill out!';
+  } else if (input == '' || !input) {
+    return 'Fine. Be that way!';
+  } else {
+    return 'Whatever.';
+  }
+
+
+
 };
 
 module.exports = Bob;
