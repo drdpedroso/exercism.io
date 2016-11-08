a8b045cf0280e3021c8407d8a94415f95ec7aac4
Eduardo Pedroso <drdpedroso@gmail.com>
2016-11-07 22:05:07 -0200

    Solving bob

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,17 +1,21 @@
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-  if(input[input.length - 1] == '?' && input != input.toUpperCase()){
-    return 'Sure.';
-  } else if (input === input.toUpperCase()) {
-    return 'Whoa, chill out!';
-  } else if (input == '' || !input) {
+  var check = /^\s*$/.test(input);
+
+  if(check){
     return 'Fine. Be that way!';
   } else {
-    return 'Whatever.';
+    if(input === input.toUpperCase() && input.toLowerCase() != input){
+      return 'Whoa, chill out!';
+    } else if (input[input.length - 1] == '?' ) {
+      return 'Sure.';
+    } else if (input == '' || !input) {
+      return 'Fine. Be that way!';
+    } else {
+      return 'Whatever.';
+    }
   }
-
-
 
 };
 
